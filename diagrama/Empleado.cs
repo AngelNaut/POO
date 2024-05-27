eea1b550d1824a6988fbd589ebf9ebc0d63753da
Angel Daniel Naut Padilla <angel_naut@hotmail.com>
2024-05-26 22:30:40 -0400

    Agregar archivos de proyecto.

---
+++ diagrama/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diagrama
+{
+    internal class Empleado
+    {
+        protected double sueldo;
+        protected 
+    }
+}
---
+++ diagrama/MiembroComunidad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diagrama
+{
+    internal abstract class MiembroComunidad
+    {
+        protected string id;
+        protected string nombre;
+        protected string apellido;
+        protected int edad;
+        protected string centroEducativo;
+
+
+    }
+}
